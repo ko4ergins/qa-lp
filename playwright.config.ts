b33a6eb239242d7748be8393a48637d7f0847816
ko4ergins1 <ko4@ukr.net>
2022-03-31 12:00:51 +0300

    allure report

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,22 +2,17 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
    testDir: 'tests',
-   reporter: 'list',
-   timeout: 60000,
+   reporter: [['allure-playwright'], ['list']],
+   timeout: 90000,
    workers: 1,
-   projects: [
-      {
-         name: `UI`,
-         use: {
-            screenshot: 'only-on-failure',
-            baseURL: 'https://pokemon.com',
-            trace: 'on-first-retry',
-            headless: false,
-            browserName: 'chromium',
-            ignoreHTTPSErrors: true,
-         },
-      },
-   ],
+   use: {
+      screenshot: 'only-on-failure',
+      baseURL: 'https://pokemon.com',
+      trace: 'on-first-retry',
+      headless: false,
+      browserName: 'chromium',
+      ignoreHTTPSErrors: true,
+   },
 };
 
 export default config;
